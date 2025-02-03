0cc4b4b061b5d341525348adcd26543aca2430f5
Nino <fazernino@gmail.com>
2025-02-03 15:15:26 +0100

    added exo with token

---
+++ exo/auth.js
@@ -0,0 +1,40 @@
+const user = {
+  email: "faz@gmail.com",
+  password: "1234",
+};
+
+const register = async (email, password) => {
+  const newUser = {
+    email,
+    password,
+  };
+
+  return generateToken(newUser);
+};
+
+function generateToken(user) {
+  const payload = JSON.stringify({ email: user.email });
+  const token = btoa(payload);
+
+  console.log(token);
+  return token;
+}
+
+const login = async (email, password) => {
+  const user = {
+    email,
+    password,
+  };
+
+  const token = generateToken(user);
+  if (verifyToken(token)) {
+    return "Login successful";
+  } else {
+    return "Email or password is incorrect";
+  }
+};
+
+function verifyToken(token) {
+  const payload = atob(token);
+  return JSON.parse(payload);
+}

f5a1072ed9d29b00af3ff955b73232b8efcd3b3b
Nino <fazernino@gmail.com>
2025-02-05 10:07:41 +0100

    added Swagger

--- serveur/src/user/dto/loginDto.ts
+++ serveur/src/user/dto/loginDto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
@@ -8,9 +9,21 @@ export class LoginDto {
         },
     )
     @IsNotEmpty()
+    @ApiProperty({
+        type: String,
+        description: 'Email',
+        example: 'test@gmail.com',
+        required: true,
+    })
     email: string;
 
     @IsNotEmpty()
     @MinLength(6, { message: 'Password must be at least 6 characters' })
+    @ApiProperty({
+        type: String,
+        description: 'Password',
+        example: 'abc123',
+        required: true,
+    })
     password: string;
 }

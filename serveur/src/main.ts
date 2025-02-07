1a54c197dbd485c13d0bb0a4993bc08db8ea2532
Nino <fazernino@gmail.com>
2025-02-07 16:32:36 +0100

    updated port

--- serveur/src/main.ts
+++ serveur/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:3000', 'https://mon-site.com'],
+        origin: ['http://localhost:5000'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         allowedHeaders: 'Content-Type, Authorization',
         credentials: true,
